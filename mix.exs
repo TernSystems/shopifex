24210f6429708388c54d2a064d3f9081253e1da3
Seth Messer <seth@megalithic.io>
2024-06-06 15:15:10 -0400

    more

--- mix.exs
+++ mix.exs
@@ -53,7 +53,7 @@ defmodule Shopifex.MixProject do
       {:phoenix, "~> 1.7.12"},
       {:phoenix_ecto, "~> 4.4"},
       {:phoenix_html, "~> 4.0"},
-      # {:phoenix_view, "~> 2.0"},
+      {:phoenix_view, "~> 2.0"},
       {:phoenix_html_helpers, "~> 1.0"},
       {:ecto_sql, "~> 3.7"},
       {:postgrex, ">= 0.0.0"},
