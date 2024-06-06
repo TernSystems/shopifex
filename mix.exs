831d3d62e308579270c2e5428279564be4e5a196
Seth Messer <seth@megalithic.io>
2024-06-06 15:11:43 -0400

    chore: further updates for latest phoenix, et al.

--- mix.exs
+++ mix.exs
@@ -54,6 +54,7 @@ defmodule Shopifex.MixProject do
       {:phoenix_ecto, "~> 4.4"},
       {:phoenix_html, "~> 4.0"},
       # {:phoenix_view, "~> 2.0"},
+      {:phoenix_html_helpers, "~> 1.0"},
       {:ecto_sql, "~> 3.7"},
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
